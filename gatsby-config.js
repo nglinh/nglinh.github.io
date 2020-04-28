3fd997e80e070e926354a7b48079f6a27bb140ac
Hien Linh Nguyen <nglinh2312@gmail.com>
2020-04-29 00:41:42 +0800

    install plugins

---
+++ gatsby-config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  siteMetadata: {
+    title: `Your Name - Blog`,
+    author: `Your Name`,
+  },
+  plugins: [
+    "gatsby-plugin-catch-links",
+    "gatsby-plugin-react-helmet",
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `${__dirname}/src/pages`,
+        name: "pages",
+      },
+    },
+    {
+      resolve: "gatsby-transformer-remark",
+      options: {
+        plugins: ["gatsby-remark-copy-linked-files", "gatsby-remark-images", "gatsby-remark-prismjs"],
+      },
+    },
+  ],
+}
